93bc05a4d0b26444c94f4e97d9bc018989250976
Timofey Krivenkov <drake9100@mail.ru>
2022-12-18 23:08:17 +0300

    ADD: Секция с шагами

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@ import "./components/burger.js";
 import "./components/info-slider.js";
 import "./components/form-selectors.js";
 import "./components/form-validation.js";
+import "./components/steps.js";
